65eba91296ad41e5dcf0e8f18b918405c1ecbc1d
ibrusentsev <IBrusentsev@spb.beeline.ru>
2018-06-26 14:19:22 +0300

    add constants, tools, load from file

---
+++ src/main/scala/system/Constants.scala
@@ -0,0 +1,11 @@
+package system
+
+object Constants {
+  private val DATA_PATH: String = "src/main/recources/sources/"
+
+  val FILE_POPULATION_FM: String = "unsd-citypopulation-year-fm.csv"
+  val FILE_POPULATION_BOTH: String = "unsd-citypopulation-year-both.csv"
+  val PATH_POPULATION_FM: String = DATA_PATH + FILE_POPULATION_FM
+  val PATH_POPULATION_BOTH: String = DATA_PATH + FILE_POPULATION_BOTH
+
+}
--- src/main/scala/system/Main.scala
+++ src/main/scala/system/Main.scala
@@ -2,14 +2,16 @@ package system
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import system.Constants._
 
 object Main extends App{
-  private val sparkConf = new SparkConf()
+  private val sparkConf = new SparkConf().setMaster("local")
   implicit val spark: SparkSession = SparkSession.builder()
     .config(sparkConf)
-    .enableHiveSupport()
     .getOrCreate()
 
-
+  val test = spark.read.format("com.databricks.spark.csv").option("header", "true").load(PATH_POPULATION_FM)
+  println(test.count())
+  test.show(20)
 
 }
