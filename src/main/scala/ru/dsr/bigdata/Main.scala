5940f197ad2c1425e42b4d1faf62f4e280394870
ibrusentsev <IBrusentsev@spb.beeline.ru>
2018-07-09 16:18:13 +0300

    fix parameters in Main

--- src/main/scala/ru/dsr/bigdata/Main.scala
+++ src/main/scala/ru/dsr/bigdata/Main.scala
@@ -1,19 +1,18 @@
 package ru.dsr.bigdata
 
-import com.typesafe.config.{Config, ConfigFactory}
+import com.typesafe.config.ConfigFactory
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
 
 object Main extends App{
   private val conf = ConfigFactory.load.getConfig("app")
-  implicit val root: Config = conf
 
   implicit val parameters: Parameters = Parameters.getInstanceOf(conf)
 
-  private val sparkConf = new SparkConf().setMaster(root.getString("spark.master"))
+  private val sparkConf = new SparkConf().setMaster(parameters.spark_master)
   implicit val spark: SparkSession = SparkSession.builder()
     .config(sparkConf)
-    .config("spark.mongodb.output.uri", root.getString("mongodb.output_uri"))
+    .config("spark.mongodb.output.uri", parameters.output_uri)
     .getOrCreate()
 
   var fm: Dataset[Row] = _
@@ -25,22 +24,23 @@ object Main extends App{
     case "url" =>
       fm = DataLoad.loadFromUrl(parameters.fm_url)
       both = DataLoad.loadFromUrl(parameters.both_url)
+    case _ =>
+      println("Wrong config load_from!")
   }
 
-  fm.show()
-  both.show()
-
-//  parameters.save_to match {
-//    case "mongodb" =>
-//      saveToMongoDB(Job.getPopulation(both), "population")
-//      saveToMongoDB(Job.getCountMillionCities(both), "countMillionCities")
-//      saveToMongoDB(Job.getTop5Cities(both), "top5Cities")
-//      saveToMongoDB(Job.getRatioPopulation(fm), "ratioPopulation")
-//    case "csv" =>
-//      saveToCsv(Job.getPopulation(both), "population.csv")
-//      saveToCsv(Job.getCountMillionCities(both), "countMillionCities.csv")
-//      saveToCsv(Job.getTop5Cities(both), "top5Cities.csv")
-//      saveToCsv(Job.getRatioPopulation(fm), "ratioPopulation.csv")
-//  }
+  parameters.save_to match {
+    case "mongodb" =>
+      Tools.saveToMongoDB(Job.getPopulation(both), "population")
+      Tools.saveToMongoDB(Job.getCountMillionCities(both), "countMillionCities")
+      Tools.saveToMongoDB(Job.getTop5Cities(both), "top5Cities")
+      Tools.saveToMongoDB(Job.getRatioPopulation(fm), "ratioPopulation")
+    case "csv" =>
+      Tools.saveToCsv(Job.getPopulation(both), "population.csv")
+      Tools.saveToCsv(Job.getCountMillionCities(both), "countMillionCities.csv")
+      Tools.saveToCsv(Job.getTop5Cities(both), "top5Cities.csv")
+      Tools.saveToCsv(Job.getRatioPopulation(fm), "ratioPopulation.csv")
+    case _ =>
+      println("Wrong config save_to!")
+  }
 
 }
